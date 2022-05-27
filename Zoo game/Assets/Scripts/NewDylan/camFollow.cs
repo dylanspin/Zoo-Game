f21e65abdb8bbd40da7a381cce55f133097b2335
Juan Salas <salasherdia@gmail.com>
2022-05-27 22:45:18 +0200

    prairie dog gazelle and elephant animations

---
+++ Zoo game/Assets/Scripts/NewDylan/camFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camFollow : MonoBehaviour
+{
+    [Header("settings")]
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float translateSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private void FixedUpdate()
+    {
+        positionLerping();
+        rotationLerping();
+    }
+
+    private void positionLerping()
+    {
+        var targetPosition = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+    }
+
+    private void rotationLerping()
+    {
+        var direction = target.position - transform.position;
+        var rotation = Quaternion.LookRotation(direction,Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+}
